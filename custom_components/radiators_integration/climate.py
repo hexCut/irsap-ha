a6946b66968afaa32d74833fe03705a87921442a
valerix85 <52428493+valerix85@users.noreply.github.com>
2024-09-27 18:45:49 +0200

    Update climate.py
    Update to avoid WARNING for 2025.1 release and fix sync

--- custom_components/radiators_integration/climate.py
+++ custom_components/radiators_integration/climate.py
@@ -1,16 +1,14 @@
 import logging
 from homeassistant.components.climate import (
     ClimateEntity,
-    HVAC_MODE_HEAT,
-    HVAC_MODE_OFF,
+    HVACMode,  # Importa HVACMode
 )
 from homeassistant.components.climate.const import ClimateEntityFeature
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import UnitOfTemperature  # Importa UnitOfTemperature
 from .const import DOMAIN, USER_POOL_ID, CLIENT_ID, REGION
 import aiohttp
 import json
 from warrant import Cognito
-import requests
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,10 +25,11 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         _LOGGER.debug("Token and envID successfully obtained. Retrieving radiators.")
         radiators = await get_radiators(token, envID)
         if radiators:
-            async_add_entities(
-                radiators, True
-            )  # Usa async_add_entities fornito dalla piattaforma
-            _LOGGER.debug(f"Created {len(radiators)} radiators.")
+            entities = [
+                RadiatorClimate(radiator, token, envID) for radiator in radiators
+            ]
+            async_add_entities(entities, True)  # Aggiungi le entità alla piattaforma
+            _LOGGER.debug(f"Created {len(entities)} radiators.")
         else:
             _LOGGER.warning("No radiators found in the API.")
     else:
@@ -106,7 +105,7 @@ def extract_device_info(
                             float(tmp_value) / 10 if tmp_value is not None else 0
                         )
 
-                    devices_info.append(RadiatorClimate(device_info))
+                    devices_info.append(device_info)
                 find_device_keys(value)
         elif isinstance(obj, list):
             for item in obj:
@@ -119,15 +118,18 @@ def extract_device_info(
 class RadiatorClimate(ClimateEntity):
     """Representation of a radiator climate entity."""
 
-    def __init__(self, radiator):
+    def __init__(self, radiator, token, envID):
         self._radiator = radiator
         self._name = f"Radiator {radiator['serial']}"
         self._unique_id = f"radiator_{radiator['serial']}"
         self._current_temperature = radiator.get("temperature", 0)
+        self._token = token
+        self._envID = envID
+        self._state = True  # Assume acceso per default
 
         # Aggiungi qui le modalità HVAC supportate
-        self._attr_hvac_modes = [HVAC_MODE_HEAT, HVAC_MODE_OFF]
-        self._attr_hvac_mode = HVAC_MODE_HEAT
+        self._attr_hvac_modes = [HVACMode.HEAT, HVACMode.OFF]  # Usa HVACMode
+        self._attr_hvac_mode = HVACMode.HEAT if self._state else HVACMode.OFF
         self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
 
     @property
@@ -143,14 +145,85 @@ class RadiatorClimate(ClimateEntity):
     @property
     def temperature_unit(self):
         """Return the unit of measurement."""
-        return TEMP_CELSIUS
+        return UnitOfTemperature.CELSIUS  # Usa UnitOfTemperature
 
     @property
     def current_temperature(self):
         """Return the current temperature."""
         return self._current_temperature
 
+    @property
+    def hvac_mode(self):
+        """Return current HVAC mode."""
+        return self._attr_hvac_mode
+
+    @property
+    def hvac_modes(self):
+        """Return available HVAC modes."""
+        return self._attr_hvac_modes
+
+    async def async_set_hvac_mode(self, hvac_mode):
+        """Set new target HVAC mode."""
+        if hvac_mode == HVACMode.OFF:
+            _LOGGER.debug(f"Setting {self._radiator['serial']} to OFF")
+            await self._set_radiator_state(False)
+        elif hvac_mode == HVACMode.HEAT:
+            _LOGGER.debug(f"Setting {self._radiator['serial']} to HEAT")
+            await self._set_radiator_state(True)
+        else:
+            _LOGGER.error(f"Unsupported HVAC mode: {hvac_mode}")
+            return
+
+        # Aggiorna la modalità HVAC attuale
+        self._attr_hvac_mode = hvac_mode
+        self.async_write_ha_state()
+
+    async def _set_radiator_state(self, state):
+        """Send request to set the radiator state."""
+        _LOGGER.debug(
+            f"Setting radiator state: {self._radiator['serial']} to {'on' if state else 'off'}"
+        )
+
+        url = "https://flqpp5xzjzacpfpgkloiiuqizq.appsync-api.eu-west-1.amazonaws.com/graphql"
+        headers = {
+            "Authorization": f"Bearer {self._token}",
+            "Content-Type": "application/json",
+        }
+
+        payload = {
+            "operationName": "UpdateShadow",
+            "variables": {
+                "envId": self._envID,
+                "payload": json.dumps(
+                    {"state": {"desired": {"PCM_ENB": 1 if state else 0}}}
+                ),
+            },
+            "query": "mutation UpdateShadow($envId: ID!, $payload: AWSJSON!) {\n asyncUpdateShadow(envId: $envId, payload: $payload) {\n status\n code\n message\n payload\n __typename\n }\n}\n",
+        }
+
+        _LOGGER.debug(f"Payload sent: {payload}")
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, headers=headers, json=payload) as response:
+                    _LOGGER.debug(
+                        f"API response: {response.status}, {await response.text()}"
+                    )
+                    if response.status == 200:
+                        _LOGGER.debug(
+                            f"Radiator state {self._radiator['serial']} updated successfully."
+                        )
+                        self._state = state
+                    else:
+                        _LOGGER.error(
+                            f"Error updating radiator state {self._radiator['serial']}: {response.status}"
+                        )
+        except Exception as e:
+            _LOGGER.error(
+                f"Error setting radiator state {self._radiator['serial']}: {e}"
+            )
+
     async def async_update(self):
         """Fetch new state data for this climate entity."""
-        _LOGGER.debug(f"Updating radiator climate {self._radiator['serial']}")
-        self._current_temperature = self._radiator.get("temperature", 0)
+        _LOGGER.info(f"Updating radiator climate {self._radiator['serial']}")
+        self._state = self._radiator["temperature"]
