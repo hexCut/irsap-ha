2b5b27e041e6e0ecf1ddd171664c377a52cef046
valerix85 <52428493+valerix85@users.noreply.github.com>
2024-10-09 17:18:25 +0200

    Update config_flow.py

--- custom_components/irsap-ha/config_flow.py
+++ custom_components/irsap-ha/config_flow.py
@@ -1,4 +1,5 @@
 import logging
+import aiohttp
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers import entity_registry as er
@@ -6,10 +7,9 @@ from homeassistant.data_entry_flow import FlowResult
 import voluptuous as vol
 from .const import DOMAIN, USER_POOL_ID, CLIENT_ID, REGION
 from warrant import Cognito
-import requests
+import asyncio
 
 _LOGGER = logging.getLogger(__name__)
-
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     "Handle a config flow for radiators integration."
@@ -33,7 +33,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         username = user_input["username"]
         password = user_input["password"]
 
-        # Qui chiamiamo la funzione per ottenere il token e l'envID
+        # Chiamiamo la funzione per ottenere il token e l'envID in modo asincrono
         token = await self.hass.async_add_executor_job(
             login_with_srp, username, password
         )
@@ -51,9 +51,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors={"base": "invalid_credentials"},
             )
 
-        envID = await self.hass.async_add_executor_job(
-            envid_with_srp, username, password, token
-        )
+        envID = await self.async_get_envID(username, password, token)
 
         if envID is None:
             _LOGGER.error("Failed to obtain envID.")
@@ -84,6 +82,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def async_get_options_flow(config_entry):
         return RadiatorsIntegrationOptionsFlow(config_entry)
 
+    async def async_get_envID(self, username, password, token):
+        """Asynchronous method to get envID."""
+        return await self.hass.async_add_executor_job(
+            envid_with_srp, username, password, token
+        )
 
 class RadiatorsIntegrationOptionsFlow(config_entries.OptionsFlow):
     "Handle the options flow for the integration."
@@ -129,9 +132,13 @@ class RadiatorsIntegrationOptionsFlow(config_entries.OptionsFlow):
 
         return self.async_create_entry(title="", data=user_input)
 
-
-def login_with_srp(username, password):
+async def login_with_srp(username, password):
     "Log in and obtain the access token using Warrant."
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, _sync_login_with_srp, username, password)
+
+def _sync_login_with_srp(username, password):
+    """Synchronous function to log in using Warrant."""
     try:
         u = Cognito(USER_POOL_ID, CLIENT_ID, username=username, user_pool_region=REGION)
         u.authenticate(password=password)
@@ -141,32 +148,30 @@ def login_with_srp(username, password):
         _LOGGER.error(f"Error during login: {e}")
         return None
 
-
-def envid_with_srp(username, password, token):
+async def envid_with_srp(username, password, token):
     "Login and obtain the envID using Warrant."
-    url = (
-        "https://flqpp5xzjzacpfpgkloiiuqizq.appsync-api.eu-west-1.amazonaws.com/graphql"
-    )
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json",
-    }
-    graphql_query = {
-        "operationName": "ListEnvironments",
-        "variables": {},
-        "query": "query ListEnvironments {\n listEnvironments {\n environments {\n envId\n envName\n userRole\n __typename\n }\n __typename\n }\n}\n",
-    }
+    async with aiohttp.ClientSession() as session:
+        url = "https://flqpp5xzjzacpfpgkloiiuqizq.appsync-api.eu-west-1.amazonaws.com/graphql"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        }
+        graphql_query = {
+            "operationName": "ListEnvironments",
+            "variables": {},
+            "query": "query ListEnvironments {\n listEnvironments {\n environments {\n envId\n envName\n userRole\n __typename\n }\n __typename\n }\n}\n",
+        }
 
-    try:
-        response = requests.post(url, headers=headers, json=graphql_query)
-        if response.status_code == 200:
-            data = response.json()
-            envId = data["data"]["listEnvironments"]["environments"][0]["envId"]
-            _LOGGER.debug(f"envId retrieved from API: {envId}")
-            return envId
-        else:
-            _LOGGER.error(f"API request error: {response.status_code}")
+        try:
+            async with session.post(url, headers=headers, json=graphql_query) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    envId = data["data"]["listEnvironments"]["environments"][0]["envId"]
+                    _LOGGER.debug(f"envId retrieved from API: {envId}")
+                    return envId
+                else:
+                    _LOGGER.error(f"API request error: {response.status}")
+                    return None
+        except Exception as e:
+            _LOGGER.error(f"Error during API call: {e}")
             return None
-    except Exception as e:
-        _LOGGER.error(f"Error during API call: {e}")
-        return None
